fe44db6115a93f1c3a63aaebd1e5ebd09768df8c
Yasminn <carvalhosyasminn@gmail.com>
2026-09-09 20:06:16 -0300

    Exercicio 1

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/sobre', function () {
+    return 'Esta é a página sobre o projeto de alunos.';
+});
+
+Route::get('/alunos', function () {
+    return 'Esta é a página de listagem de alunos.';
+});
+
+Route::get('/contato', function () {
+    return 'Entre em contato: contato@escola.com';
 });

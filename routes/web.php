343dec6194f3c0d4595bf06c05b179447888a0df
Yasminn <carvalhosyasminn@gmail.com>
2026-09-09 20:42:45 -0300

    atv1

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
 Route::get('/sobre', function () {
     return 'Esta é a página sobre o projeto de alunos.';
 });

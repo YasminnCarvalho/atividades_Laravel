0740a6f6b2433219894d13a9abe67d396fad734c
Yasminn <carvalhosyasminn@gmail.com>
2026-09-09 21:33:28 -0300

    correção

--- app/Http/Controllers/AlunoController.php
+++ app/Http/Controllers/AlunoController.php
@@ -6,5 +6,43 @@ use Illuminate\Http\Request;
 
 class AlunoController extends Controller
 {
-    //
+    public function index()
+    {
+        return "Listagem de alunos";
+    }
+
+
+    public function create()
+    {
+        return "Formulário de criação de aluno";
+    }
+
+    public function store(Request $request)
+    {
+        return "Aluno salvo com sucesso";
+    }
+
+
+    public function show($id)
+    {
+        return "Detalhes do aluno de ID: $id";
+    }
+
+
+    public function edit($id)
+    {
+        return "Formulário de edição do aluno de ID: $id";
+    }
+
+
+    public function update(Request $request, $id)
+    {
+        return "Aluno de ID: $id atualizado com sucesso";
+    }
+
+
+    public function destroy($id)
+    {
+        return "Aluno de ID: $id apagado com sucesso";
+    }
 }

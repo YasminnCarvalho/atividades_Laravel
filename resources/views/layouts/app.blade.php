2bf8b8db5cfd9564c5b3847b1df1b25204c087b6
Yasminn <carvalhosyasminn@gmail.com>
2026-09-09 21:36:29 -0300

    atv 7

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Projeto Alunos</title>
+</head>
+<body>
+
+    @include('partials.menu')
+
+    <main>
+        @yield('conteudo')
+    </main>
+
+</body>
+</html>
